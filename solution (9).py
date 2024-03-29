c91bc1402a537919ec285a9ef710e0fcd3fad142
GermanDek <158293124+GermanDek@users.noreply.github.com>
2024-03-29 09:45:20 +0300

    модуль 1 задание 3

---
+++ solution (9).py
@@ -0,0 +1,11 @@
+#coding: utf-8
+low = int(input())
+high = int(input())
+between = True
+
+while line := input(): 
+
+         if between:
+             between = low <= int(line) <= high
+
+print(between)
